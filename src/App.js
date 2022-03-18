d8e54441bc999c4a3ba9bd5d020edd64be359067
unknown <Administrator@IT-CIEMIEGAK.brwsa.com.pl>
2022-03-18 14:30:24 +0100

    changes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Zad1 } from "./components/Zad1";
+
+function App() {
+  return (
+    <div>
+      <Zad1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Zad1.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const initialData = {
+  human: {
+    sex: "man",
+    surname: "Detic",
+  },
+  accountBalance: 1000,
+};
+
+export const Zad1 = () => {
+  const [initialState, setInitialState] = useState(initialData);
+
+  const handleBuy = () => {
+    setInitialState((prevValue) => ({
+      ...prevValue,
+      accountBalance: prevValue.accountBalance - 500,
+    }));
+  };
+  const handleSell = () => {
+    setInitialState((prevValue) => ({
+      ...prevValue,
+      accountBalance: prevValue.accountBalance + 500,
+    }));
+  };
+
+  const handleSexChange = () => {
+    setInitialState((prevValue) => ({
+      ...prevValue,
+    }));
+  };
+
+  return (
+    <div>
+      <p>{initialState.human.sex}</p>
+      <p>{initialState.human.surname}</p>
+      <div>
+        <p>{initialState.accountBalance}</p>
+        <button onClick={handleBuy}>Buy</button>
+        <button onClick={handleSell}>Sell</button>
+        <button onClick={handleSexChange}>changeSex</button>
+      </div>
+    </div>
+  );
+};
